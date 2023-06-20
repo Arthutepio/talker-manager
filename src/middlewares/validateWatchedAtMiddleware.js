91806983a72fb5b6826f0676bfb952b9a784d3b3
arthutepio <arthurodriguesr10@gmail.com>
2023-06-19 21:46:05 -0300

    fix: remove comments

--- src/middlewares/validateWatchedAtMiddleware.js
+++ src/middlewares/validateWatchedAtMiddleware.js
@@ -2,7 +2,7 @@ const validateWatchedAtMiddleware = (req, res, next) => {
   const { talk: { watchedAt } } = req.body;
   const regex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
   const date = regex.test(watchedAt);
-  // console.log(date);
+
   if (!watchedAt) {
     return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
   }
